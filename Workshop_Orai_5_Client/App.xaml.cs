5ab78469fe923117568e1cb1689961e458941ca9
hallgato <hallgato@LABOR113-01>
2024-03-18 14:38:54 +0100

    Add project files.

---
+++ Workshop_Orai_5_Client/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Workshop_Orai_5_Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
